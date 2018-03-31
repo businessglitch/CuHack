10828031d105cc59ad94b81f35b6ee14dc29d112
jenishzalavadiya <jenish.zalavadiya@gmail.com>
2018-03-31 16:49:09 -0400

    Updated Tickets attributes

--- Concert/src/assets/models/tickets/ticket.interface.ts
+++ Concert/src/assets/models/tickets/ticket.interface.ts
@@ -3,11 +3,11 @@ import { Concert }  from '../concerts/concert.interface';
 
 export interface Ticket {
     image: string;
-    title: string;
     description: string;
     price: number;
     favourite: boolean;
     favItemClass: string;
     sellerId: number;
     concert: Concert;
+    Id: number;
 }
