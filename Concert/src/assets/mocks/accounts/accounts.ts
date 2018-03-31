0d95e7be341216bd34ee0bc76101a019fd700bb3
jenishzalavadiya <jenish.zalavadiya@gmail.com>
2018-03-31 17:07:53 -0400

    Modified the mocks

--- Concert/src/assets/mocks/accounts/accounts.ts
+++ Concert/src/assets/mocks/accounts/accounts.ts
@@ -1,7 +1,7 @@
 import { Account } from "../../models/accounts/account.interface";
 
 const accountsList: Account[] = [
-    {email:"admin@admin.com",password: "admin", address:" 3325 Uplands drive", id: 1}  
+    {name:"Fahad Hayat",email:"admin@admin.com",password: "admin", address:" 3325 Uplands drive", id: 1, sold:[], bought:[], pending:[]}  
 ]   
 
  export const ACCOUNTS_LIST = accountsList;
