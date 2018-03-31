5215787e3a5d612361716af8a2c58f2274e7a4a0
jenishzalavadiya <jenish.zalavadiya@gmail.com>
2018-03-31 16:36:50 -0400

    Updated Accounts attributes

--- Concert/src/assets/models/accounts/account.interface.ts
+++ Concert/src/assets/models/accounts/account.interface.ts
@@ -1,6 +1,10 @@
 export interface Account {
+	name: string;
     email: string;
     password: string;
     address: string;
     id: number;
+    sold: number[];
+    bought: number[];
+    pending: number[];
 }
